21f7941ae05e55343f3960650d0f1f3dd0937086
Raymond Wu <raymonwu@umich.edu>
2022-06-07 21:44:22 -0400

    got date filtering working

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
 
   const [fileName, setFileName] = useState("");
   const [sheetData, setSheetData] = useState([]);
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [dateFiltered, setDateFiltered] = useState([]);
 
   const handleFileAsync = async (e) => {
     const file = e.target.files[0];
@@ -17,8 +18,11 @@ function App() {
     const data = await file.arrayBuffer();
     const workbook = XLSX.read(data);
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-
+    const jsonData = XLSX.utils.sheet_to_json(worksheet);
+    //adjust for 1 day negative offset
+    for (var i = 0; i < jsonData.length; i++) {
+      jsonData[i][Object.keys(jsonData[i])[1]] += 1;
+    }
     setSheetData(jsonData);
   };
 
@@ -27,6 +31,75 @@ function App() {
   };
   const handleEndDateOnChange = (e) => {
     setEndDate(e.target.value);
+  };
+
+  function findFirstIntegerValue(dataset) {
+    for (const index in dataset) {
+      if (typeof dataset[index] == "number") {
+        if (dataset[index] > 10000) {
+          return dataset[index];
+        }
+      }
+    }
+  }
+
+  function getMonthFromString(mon) {
+    var months = [
+      "",
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return months.indexOf(mon);
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (!startDate || !endDate) {
+      alert("Please enter both start and end dates");
+    } else if (sheetData.length === 0) {
+      alert("Please upload a valid excel file");
+    } else {
+      var filtered = sheetData.filter((issue) => {
+        return (
+          //year
+          ExcelDateToJSDate(issue[Object.keys(issue)[1]]).toString().slice(11, 15) >=
+            startDate.slice(0, 4) &&
+          ExcelDateToJSDate(issue[Object.keys(issue)[1]]).toString().slice(11, 15) <=
+            endDate.slice(0, 4) &&
+          //month
+          getMonthFromString(
+            ExcelDateToJSDate(issue[Object.keys(issue)[1]]).toString().slice(4, 7)
+          ) >= startDate.slice(5, 7) &&
+          getMonthFromString(
+            ExcelDateToJSDate(issue[Object.keys(issue)[1]]).toString().slice(4, 7)
+          ) <= endDate.slice(5, 7) &&
+          //day
+          ExcelDateToJSDate(issue[Object.keys(issue)[1]]).toString().slice(8, 10) >=
+            startDate.slice(8) &&
+          ExcelDateToJSDate(issue[Object.keys(issue)[1]]).toString().slice(8, 10) <=
+            endDate.slice(8)
+        );
+      });
+      setDateFiltered(filtered);
+      console.log(filtered);
+      console.log(
+        ExcelDateToJSDate(sheetData[3][Object.keys(sheetData[3])[1]])
+          .toString()
+          .slice(8, 10)
+      );
+      console.log(startDate.slice(8));
+      console.log(endDate);
+    }
   };
 
   const xport = React.useCallback(async () => {
@@ -41,8 +114,6 @@ function App() {
   function ExcelDateToJSDate(date) {
     return new Date(Math.round((date - 25569) * 86400 * 1000));
   }
-
-  console.log(sheetData);
 
   return (
     <div className="App">
@@ -64,13 +135,13 @@ function App() {
       Select end date to filter:
       <input type="date" className="end-date" onChange={handleEndDateOnChange} />
       {endDate}
-      {sheetData.length > 0 &&
-        sheetData
-          .reverse()
-          .splice(1, 5000)
-          .map((issue) => {
-            return <IssueCard issue={issue} />;
-          })}
+      <button type="button" onClick={handleOnSubmit}>
+        Search
+      </button>
+      {dateFiltered.length > 0 &&
+        dateFiltered.reverse().map((issue) => {
+          return <IssueCard issue={issue} />;
+        })}
       <table id="Table2XLSX">
         <tbody>
           <tr>
--- src/Components/IssueCard.js
+++ src/Components/IssueCard.js
@@ -1,12 +1,17 @@
 const IssueCard = ({ issue }) => {
   function ExcelDateToJSDate(date) {
-    return new Date(Math.round((date - 25569) * 86400 * 1000));
+    var hmm = new Date(Math.round((date - 25569) * 86400 * 1000));
+    return hmm;
   }
 
+  // {`${ExcelDateToJSDate(issue[2]).toISOString().slice(0, 10)}`}
+  // console.log(ExcelDateToJSDate(issue[Object.keys(issue)[1]]));
   return (
     <div className="issue-container">
-      Issue Date:
-      {`${ExcelDateToJSDate(issue[2]).toISOString().slice(0, 10)}`} {issue[4]} {issue[6]}
+      {issue[Object.keys(issue)[0]]} {issue[Object.keys(issue)[1]]}
+      {`${ExcelDateToJSDate(issue[Object.keys(issue)[1]]).toString().slice(0, 15)}`}{" "}
+      {/* {`${ExcelDateToJSDate(issue[Object.keys(issue)[1]]).toISOString().slice(0, 10)}`} */}
+      {issue[Object.keys(issue)[2]]} {issue[Object.keys(issue)[3]]}{" "}
       {/* {issue[0]}
       <div className="break"></div>
       {issue[1]}
