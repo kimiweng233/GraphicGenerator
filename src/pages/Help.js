aaa7823b6bc1dd6d1c83ca104ecfb599a2edda46
Raymond Wu <raymonwu@umich.edu>
2022-06-21 22:08:10 -0400

    routing

---
+++ src/pages/Help.js
@@ -0,0 +1,15 @@
+import "../css/Help.css";
+
+const Help = () => {
+  return (
+    <div className="help-container">
+      <h1>How to use Graphic Generator</h1>
+      <p>1. Select a valid excel file</p>
+      <p>2. Select a start date of earliest desired entries</p>
+      <p>3. Select an end date of latest desired entries</p>
+      <p>4. Click Search</p>
+    </div>
+  );
+};
+
+export default Help;
